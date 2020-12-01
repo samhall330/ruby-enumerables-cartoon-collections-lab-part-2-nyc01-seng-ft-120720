c029cb0e644ca78b7bf590e3e3a6069d5efe99ce
Sam Hall <mann.samantha11@gmail.com>
2020-12-01 02:44:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,6 +16,7 @@ end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  planeteer_calls.include?(valid_calls)
+  if planeteer_calls.find {|call| call == valid_calls}
+    
 
 end
